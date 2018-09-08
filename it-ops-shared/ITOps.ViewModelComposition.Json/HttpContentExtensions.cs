2e20a183df7ad0a3174de264d04eea2665cbacef
Mauro Servienti <mauro.servienti@gmail.com>
2018-09-08 17:32:05 +0200

    allows deserialization to multiple types

--- it-ops-shared/ITOps.ViewModelComposition.Json/HttpContentExtensions.cs
+++ it-ops-shared/ITOps.ViewModelComposition.Json/HttpContentExtensions.cs
@@ -7,10 +7,13 @@ namespace ITOps.ViewModelComposition.Json
 {
     public static class HttpContentExtensions
     {
-        public static async Task<ExpandoObject> AsExpando(this HttpContent content) 
-            => JsonConvert.DeserializeObject<ExpandoObject>(await content.ReadAsStringAsync(), CamelCaseToPascalSettings.GetSerializerSettings());
-        
-        public static async Task<ExpandoObject[]> AsExpandoArray(this HttpContent content) 
-            => JsonConvert.DeserializeObject<ExpandoObject[]>(await content.ReadAsStringAsync(), CamelCaseToPascalSettings.GetSerializerSettings());
+        public static async Task<T> As<T>(this HttpContent content)
+            => JsonConvert.DeserializeObject<T>(await content.ReadAsStringAsync(), CamelCaseToPascalSettings.GetSerializerSettings());
+
+        public static async Task<ExpandoObject> AsExpando(this HttpContent content)
+            => await As<ExpandoObject>(content);
+
+        public static async Task<ExpandoObject[]> AsExpandoArray(this HttpContent content)
+            => await As<ExpandoObject[]>(content);
     }
 }
