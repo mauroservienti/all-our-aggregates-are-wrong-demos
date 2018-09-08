0253daf1299cc23365ddf519fe6e4b0fcd8aef5b
Mauro Servienti <mauro.servienti@gmail.com>
2018-09-08 17:42:19 +0200

    shared NSB config

---
+++ NServiceBus.Shared/CommonEndpointSettings.cs
@@ -0,0 +1,19 @@
+﻿namespace NServiceBus.Shared
+{
+    public static class CommonEndpointSettings
+    {
+        public static void ApplyCommonConfiguration(this EndpointConfiguration config)
+        {
+            config.UseSerialization<NewtonsoftSerializer>();
+            config.UseTransport<LearningTransport>();
+
+            config.AuditProcessedMessagesTo("audit");
+            config.SendFailedMessagesTo("error");
+
+            var messageConventions = config.Conventions();
+            messageConventions.DefiningMessagesAs(t => t.Namespace != null && t.Namespace.EndsWith(".Messages"));
+            messageConventions.DefiningEventsAs(t => t.Namespace != null && t.Namespace.EndsWith(".Messages.Events"));
+            messageConventions.DefiningCommandsAs(t => t.Namespace != null && t.Namespace.EndsWith(".Messages.Commands"));
+        }
+    }
+}
