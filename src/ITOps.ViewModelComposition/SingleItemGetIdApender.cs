8ddbe394f1b14c5239bd6e52a5a61363f751d23e
Mauro Servienti <mauro.servienti@gmail.com>
2018-09-09 10:28:37 +0200

    ITOps composition

---
+++ src/ITOps.ViewModelComposition/SingleItemGetIdApender.cs
@@ -0,0 +1,26 @@
+﻿using ServiceComposer.ViewModelComposition;
+using ServiceComposer.ViewModelComposition.Json;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Warehouse.ViewModelComposition
+{
+    class SingleItemGetIdApender : IHandleRequests
+    {
+        public bool Matches(RouteData routeData, string httpVerb, HttpRequest request)
+        {
+            return HttpMethods.IsGet(httpVerb)
+                   && routeData.Values.ContainsKey("id");
+        }
+
+        public Task Handle(string requestId, dynamic vm, RouteData routeData, HttpRequest request)
+        {
+            var id = (string)routeData.Values["id"];
+            vm.Id = id;
+
+            return Task.CompletedTask;
+        }
+    }
+}
