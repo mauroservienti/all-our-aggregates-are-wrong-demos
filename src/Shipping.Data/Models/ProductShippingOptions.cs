a0f40f6044010382e729a321c1ce293032ae440f
Mauro Servienti <mauro.servienti@gmail.com>
2018-09-10 09:16:59 +0200

    simple refactoring

--- src/Shipping.Data/Models/ProductShippingOptions.cs
+++ src/Shipping.Data/Models/ProductShippingOptions.cs
@@ -4,22 +4,14 @@ namespace Shipping.Data.Models
 {
     public class ProductShippingOptions
     {
-        public ProductShippingOptions()
-        {
-            this.Options = new List<ShippingOption>();
-        }
-
         public int Id { get; set; }
-
-        public List<ShippingOption> Options { get; set; }
+        public List<ShippingOption> Options { get; set; } = new List<ShippingOption>();
     }
 
     public class ShippingOption
     {
         public int Id { get; set; }
-
         public int ProductShippingOptionsId { get; set; }
-
         public string Option { get; set; }
     }
 }
