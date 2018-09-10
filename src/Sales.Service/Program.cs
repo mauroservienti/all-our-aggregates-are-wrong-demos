e6adcdaa782328977daa101346027c259efa2ed9
Mauro Servienti <mauro.servienti@gmail.com>
2018-09-10 11:41:18 +0200

    endpoint config refactoring

--- src/Sales.Service/Program.cs
+++ src/Sales.Service/Program.cs
@@ -8,12 +8,13 @@ namespace Sales.Service
     {
         static async Task Main(string[] args)
         {
-            var config = new EndpointConfiguration("Sales.Service");
+            var serviceName = typeof(Program).Namespace;
+            var config = new EndpointConfiguration(serviceName);
             config.ApplyCommonConfiguration();
 
             var endpointInstance = await Endpoint.Start(config);
 
-            Console.WriteLine("Sales.Service sarted. Press any key to stop.");
+            Console.WriteLine($"{serviceName} sarted. Press any key to stop.");
             Console.ReadLine();
 
             await endpointInstance.Stop();
