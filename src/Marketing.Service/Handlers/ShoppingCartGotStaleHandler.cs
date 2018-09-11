c06250682c310be6e6490a7a5323fc23d331e4a6
Mauro Servienti <mauro.servienti@gmail.com>
2018-09-11 13:48:42 -0600

    Marketing reaction to stale carts

---
+++ src/Marketing.Service/Handlers/ShoppingCartGotStaleHandler.cs
@@ -0,0 +1,19 @@
+﻿using NServiceBus;
+using Sales.Messages.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Marketing.Service.Handlers
+{
+    class ShoppingCartGotStaleHandler : IHandleMessages<ShoppingCartGotStale>
+    {
+        public Task Handle(ShoppingCartGotStale message, IMessageHandlerContext context)
+        {
+            Console.WriteLine("Cart got stale, let's annoy user with an email :-)");
+
+            return Task.CompletedTask;
+        }
+    }
+}
