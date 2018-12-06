a5a7e6ff79d3c0411cab422d978aaff72f46283d
Mauro Servienti <mauro.servienti@gmail.com>
2018-12-06 06:50:30 +0100

    Pass in repo root path

--- build.cmd
+++ build.cmd
@@ -1,6 +1,6 @@
 @echo Off
 cd %~dp0
 
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '.\targets\ensure-required-sdk-version.ps1' -repo_root='" + %cd% + "'"
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '.\targets\ensure-required-sdk-version.ps1' -repo_root '" %cd% "'"
 
 dotnet run --project targets --no-launch-profile -- %*
