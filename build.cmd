ed7fc3b08096a15b0cb824cbedada9cfc1a4c434
Mauro Servienti <mauro.servienti@gmail.com>
2018-12-06 07:07:03 +0100

    Echoes ps exit code

--- build.cmd
+++ build.cmd
@@ -2,5 +2,6 @@
 cd %~dp0
 
 PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '.\targets\ensure-required-sdk-version.ps1'"
+echo %ERRORLEVEL%
 
 dotnet run --project targets --no-launch-profile -- %*
