f3b80f3487c38a762730d4047d8726ff7a00387e
Mauro Servienti <mauro.servienti@gmail.com>
2018-12-06 07:27:08 +0100

    Error level

--- build.cmd
+++ build.cmd
@@ -3,9 +3,10 @@ cd %~dp0
 
 PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '.\targets\ensure-required-sdk-version.ps1'"
 
-if errorlevel 0 goto build
-if errorlevel 1 goto add_env_var
-if errorlevel -1 goto SDK_mismatch
+echo %errorlevel%
+if %errorlevel% EQU 0 goto build
+if %errorlevel% EQU 1 goto add_env_var
+if %errorlevel% EQU -1 goto SDK_mismatch
 
 :add_env_var
 set DOTNET_INSTALL_DIR=%cd%"\.dotnet"
