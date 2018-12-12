9a5e4168d1ffe4114344b61b8b9d3b7db3d16de7
Mauro Servienti <mauro.servienti@gmail.com>
2018-12-12 06:41:45 +0100

    Refactor SDK installation to support multiple platforms

--- targets/Program.cs
+++ targets/Program.cs
@@ -20,7 +20,7 @@ internal class Program
             {
                 var (customSdk, sdkPath, sdkVersion) = EnsureRequiredSdkIsInstalled();
                 Console.WriteLine($"Build will be executed using {(customSdk ? "user defined SDK" : "default SDK")}, Version '{sdkVersion}'.{(customSdk ? $" Installed at '{sdkPath}'" : "")}");
-                Run($"{sdkPath}dotnet", $"build \"{solution}\" --configuration Debug");
+                Run($"{Path.Combine(sdkPath, "dotnet")}", $"build \"{solution}\" --configuration Debug");
             });
 
         Target("verify-OS-is-suppported",
@@ -42,35 +42,44 @@ internal class Program
 
         if (string.IsNullOrWhiteSpace(requiredSdkFile))
         {
-            Console.WriteLine("No custom SDK is required.");
+            Console.WriteLine("No custom SDK is required.", Color.Green);
             return (false, "", currentSdkVersion);
         }
 
         var requiredSdkVersion = File.ReadAllText(requiredSdkFile).TrimEnd(Environment.NewLine.ToCharArray());
 
-        switch (string.Compare(currentSdkVersion, requiredSdkVersion))
+        if (string.Compare(currentSdkVersion, requiredSdkVersion) == 0)
         {
-            case 0: //equals
-                throw new InvalidOperationException("Insalled SDK is the same as required one, '.required-sdk' file is not necessary.");
-
-            case 1: //current is greater
-                Console.WriteLine($"Installed SDK ({currentSdkVersion}) is more recent than required one ({requiredSdkVersion}).", Color.Yellow);
-                Console.WriteLine($"{requiredSdkVersion} will be installed and and used to run the build", Color.Yellow);
-                break;
-            case -1: //required is greater
-                Console.WriteLine($"Required SDK ({requiredSdkVersion}) is more recent than installed one ({currentSdkVersion}).", Color.Green);
-                Console.WriteLine($"{requiredSdkVersion} will be installed and and used to run the build", Color.Green);
-                break;
+            Console.WriteLine("Insalled SDK is the same as required one, '.required-sdk' file is not necessary. Build will use the SDK available on the machine.", Color.Yellow);
+            return (false, "", currentSdkVersion);
         }
 
-        Console.WriteLine("Downloading dotnet-install.ps1 script.");
+        Console.WriteLine($"Installed SDK ({currentSdkVersion}) doesn't match required one ({requiredSdkVersion}).", Color.Yellow);
+        Console.WriteLine($"{requiredSdkVersion} will be installed and and used to run the build.", Color.Yellow);
+
+        var installScriptName = RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
+            ? "dotnet-install.ps1"
+            : "dotnet-install.sh";
+
+        var installScriptUrl = $"https://dot.net/v1/{installScriptName}";
+
+        Console.WriteLine($"Downloading {installScriptName} script, from {installScriptUrl}.");
 
         Directory.CreateDirectory(buildSupportDir);
-        new WebClient().DownloadFile("https://dot.net/v1/dotnet-install.ps1", $@"{buildSupportDir}\dotnet-install.ps1");
+        new WebClient().DownloadFile(installScriptUrl, Path.Combine(buildSupportDir, installScriptName));
 
         Console.WriteLine($"Ready to install custom SDK, version {requiredSdkVersion}.");
-        Run("powershell", $@".\{buildSupportDir}\dotnet-install.ps1 -Version {requiredSdkVersion} -InstallDir {customSdkInstallDir}");
 
-        return (true, $@"{customSdkInstallDir}\", requiredSdkVersion);
+        var installScriptLocation = Path.Combine(".", buildSupportDir, installScriptName);
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            Run("powershell", $@"{installScriptLocation} -Version {requiredSdkVersion} -InstallDir {customSdkInstallDir}");
+        }
+        else
+        {
+            Run("bash", $@"{installScriptLocation} --version {requiredSdkVersion} --install-dir {customSdkInstallDir}");
+        }
+
+        return (true, customSdkInstallDir, requiredSdkVersion);
     }
 }
