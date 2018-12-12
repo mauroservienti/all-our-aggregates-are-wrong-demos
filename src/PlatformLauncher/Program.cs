2113cb8d106ac1380449bcc4aec5193e42953a8b
Mauro Servienti <mauro.servienti@gmail.com>
2018-12-12 10:30:39 +0100

    Add Platform Launcher

---
+++ src/PlatformLauncher/Program.cs
@@ -0,0 +1,13 @@
+namespace PlatformLauncher
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "PlatformLauncher";
+            Particular.PlatformLauncher.Launch();
+        }
+    }
+}
