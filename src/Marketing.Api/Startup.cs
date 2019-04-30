587cc98323efe4026f539f9e51dc398cc20ec849
Mauro Servienti <mauro.servienti@gmail.com>
2019-04-30 07:56:16 +0200

    remove not needed Startup ctor(s)

--- src/Marketing.Api/Startup.cs
+++ src/Marketing.Api/Startup.cs
@@ -1,20 +1,12 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Marketing.Api
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddCors(options =>
--- src/WebApp/Startup.cs
+++ src/WebApp/Startup.cs
@@ -1,7 +1,6 @@
 ﻿using ITOps.Middlewares;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using ServiceComposer.AspNetCore;
