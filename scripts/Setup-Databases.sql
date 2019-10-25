92764d24e519a6ffb13722ec61a17b13d225e78e
Mauro Servienti <mauro.servienti@gmail.com>
2019-10-25 09:38:04 +0200

    Fix DB creation scripts

--- scripts/Setup-Databases.sql
+++ scripts/Setup-Databases.sql
@@ -2,21 +2,21 @@ USE [master]
 GO
 
 :setvar DatabaseName "Sales"
-IF NOT EXISTS (SELECT name FROM master.dbo.sysdatabases WHERE name = N'$(DatabaseName)')
-CREATE DATABASE [$(DatabaseName)] ON ( NAME = N'$(DatabaseName)', FILENAME = N'$(UserPath)\$(DatabaseName).mdf' )
+IF NOT EXISTS (SELECT name FROM master.dbo.sysdatabases WHERE name = '$(DatabaseName)')
+CREATE DATABASE [$(DatabaseName)] ON ( NAME = '$(DatabaseName)', FILENAME = '$(UserPath)\$(DatabaseName).mdf' )
 GO
 
 :setvar DatabaseName "Warehouse"
-IF NOT EXISTS (SELECT name FROM master.dbo.sysdatabases WHERE name = N'$(DatabaseName)')
-CREATE DATABASE [$(DatabaseName)] ON ( NAME = N'$(DatabaseName)', FILENAME = N'$(UserPath)\$(DatabaseName).mdf' )
+IF NOT EXISTS (SELECT name FROM master.dbo.sysdatabases WHERE name = '$(DatabaseName)')
+CREATE DATABASE [$(DatabaseName)] ON ( NAME = '$(DatabaseName)', FILENAME = '$(UserPath)\$(DatabaseName).mdf' )
 GO
 
 :setvar DatabaseName "Shipping"
-IF NOT EXISTS (SELECT name FROM master.dbo.sysdatabases WHERE name = N'$(DatabaseName)')
-CREATE DATABASE [$(DatabaseName)] ON ( NAME = N'$(DatabaseName)', FILENAME = N'$(UserPath)\$(DatabaseName).mdf' )
+IF NOT EXISTS (SELECT name FROM master.dbo.sysdatabases WHERE name = '$(DatabaseName)')
+CREATE DATABASE [$(DatabaseName)] ON ( NAME = '$(DatabaseName)', FILENAME = '$(UserPath)\$(DatabaseName).mdf' )
 GO
 
 :setvar DatabaseName "Marketing"
-IF NOT EXISTS (SELECT name FROM master.dbo.sysdatabases WHERE name = N'$(DatabaseName)')
-CREATE DATABASE [$(DatabaseName)] ON ( NAME = N'$(DatabaseName)', FILENAME = N'$(UserPath)\$(DatabaseName).mdf' )
+IF NOT EXISTS (SELECT name FROM master.dbo.sysdatabases WHERE name = '$(DatabaseName)')
+CREATE DATABASE [$(DatabaseName)] ON ( NAME = '$(DatabaseName)', FILENAME = '$(UserPath)\$(DatabaseName).mdf' )
 GO
