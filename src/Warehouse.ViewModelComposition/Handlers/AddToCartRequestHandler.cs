729c8cd4380b94da5abe381f4db7de6ffcb63423
mauroservienti <mauro.servienti@gmail.com>
2019-10-28 17:35:59 +0100

    Handle warehouse add to cart

---
+++ src/Warehouse.ViewModelComposition/Handlers/AddToCartRequestHandler.cs
@@ -0,0 +1,21 @@
+﻿using NServiceBus;
+using Sales.ViewModelComposition.Messages;
+using System.Threading.Tasks;
+using Warehouse.Messages.Commands;
+
+namespace Warehouse.ViewModelComposition.Handlers
+{
+    class AddToCartRequestHandler : IHandleMessages<AddToCartRequest>
+    {
+        public Task Handle(AddToCartRequest message, IMessageHandlerContext context)
+        {
+            return context.Send("Warehouse.Service", new AddItemToCart()
+            {
+                CartId = message.CartId,
+                RequestId = message.RequestId,
+                ProductId = int.Parse(message.RequestData["warehouse-product-id"]),
+                Quantity = int.Parse(message.RequestData["warehouse-quantity"]),                
+            });
+        }
+    }
+}
