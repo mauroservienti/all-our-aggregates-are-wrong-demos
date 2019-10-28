7305b3444dbbace926ea6fa0c164217dc6731973
mauroservienti <mauro.servienti@gmail.com>
2019-10-28 17:33:52 +0100

    Handle adding to cart via message

---
+++ src/Sales.Messages/Commands/AddItemToCart.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sales.Messages.Commands
+{
+    public class AddItemToCart
+    {
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+        public Guid CartId { get; set; }
+        public string RequestId { get; set; }
+    }
+}
