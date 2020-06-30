908df3a21f45782ff25c44c75c7ee83bbf252631
Mauro Servienti <mauro.servienti@gmail.com>
2020-06-30 06:43:53 +0200

    Add a ViewModel preview handler to set the ID whenever found in Route Data

---
+++ src/ITOps.ViewModelComposition/IdSetterViewModelPreviewHandler.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using ServiceComposer.AspNetCore;
+using System.Threading.Tasks;
+
+namespace ITOps.ViewModelComposition
+{
+    class IdSetterViewModelPreviewHandler : IViewModelPreviewHandler
+    {
+        Task IViewModelPreviewHandler.Preview(dynamic viewModel)
+        {
+            throw new System.NotImplementedException();
+        }
+        
+        public Task Preview(HttpRequest request, dynamic viewModel)
+        {
+            var routeData = request.HttpContext.GetRouteData();
+            if (routeData.Values.ContainsKey("id"))
+            {
+                var id = (string)routeData.Values["id"];
+                viewModel.Id = id;
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
--- src/WebApp/Startup.cs
+++ src/WebApp/Startup.cs
@@ -2,7 +2,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using ServiceComposer.AspNetCore;
-using ServiceComposer.AspNetCore.Mvc;
 
 namespace WebApp
 {
