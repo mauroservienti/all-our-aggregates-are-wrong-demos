45ebff5e6eaca1649cdb43db27fe2bd420f9dce5
Mauro Servienti <mauro.servienti@gmail.com>
2022-03-24 15:14:01 +0100

    Add initial support for a standalone composition gateway

---
+++ src/CompositionGateway/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using NServiceBus;
+
+namespace CompositionGateway
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureLogging((hostingContext, loggingBuilder) =>
+                {
+                    loggingBuilder.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                })
+                .UseNServiceBus(ctx =>
+                {
+                    var endpointConfiguration = new EndpointConfiguration("WebApp");
+                    endpointConfiguration.ApplyCommonConfiguration();
+
+                    return endpointConfiguration;
+                })
+                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+    }
+}
---
+++ src/CompositionGateway/Startup.cs
@@ -0,0 +1,27 @@
+﻿using ITOps.Middlewares;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using ServiceComposer.AspNetCore;
+
+namespace CompositionGateway
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRouting();
+            services.AddViewModelComposition(options =>
+            {
+                options.EnableWriteSupport();
+            });
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseMiddleware<ShoppingCartMiddleware>();
+
+            app.UseRouting();
+            app.UseEndpoints(endpoints => endpoints.MapCompositionHandlers());
+        }
+    }
+}
