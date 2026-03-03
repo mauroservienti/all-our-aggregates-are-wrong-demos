fbe678ef7fa685e6fa7fbc5ca77de5075271ff68
Mauro Servienti <mauro.servienti@gmail.com>
2026-03-03 06:50:12 +0100

    Use RabbitMQ

--- src/NServiceBus.Shared/CommonEndpointSettings.cs
+++ src/NServiceBus.Shared/CommonEndpointSettings.cs
@@ -2,60 +2,67 @@
 using Npgsql;
 using NpgsqlTypes;
 using NServiceBus.TransactionalSession;
+
 namespace NServiceBus
 {
     public static class CommonEndpointSettings
     {
-        public static void ApplyCommonConfiguration(this EndpointConfiguration config) 
+        extension(EndpointConfiguration config)
         {
-            config.AuditProcessedMessagesTo("audit");
-            config.SendFailedMessagesTo("error");
-
-            config.UseSerialization<NewtonsoftJsonSerializer>();
-            var transportConfig = config.UseTransport<LearningTransport>();
-            transportConfig.Transactions(TransportTransactionMode.ReceiveOnly);
-
-            var messageConventions = config.Conventions();
-            messageConventions.DefiningMessagesAs(t => t.Namespace != null && t.Namespace.EndsWith(".Messages"));
-            messageConventions.DefiningEventsAs(t => t.Namespace != null && t.Namespace.EndsWith(".Messages.Events"));
-            messageConventions.DefiningCommandsAs(t => t.Namespace != null && t.Namespace.EndsWith(".Messages.Commands"));
-
-            config.SendHeartbeatTo(
-                serviceControlQueue: "Particular.ServiceControl",
-                frequency: TimeSpan.FromSeconds(10),
-                timeToLive: TimeSpan.FromSeconds(5));
-
-            var metrics = config.EnableMetrics();
-            metrics.SendMetricDataToServiceControl(
-                serviceControlMetricsAddress: "Particular.Monitoring",
-                interval: TimeSpan.FromSeconds(5));
-        }
-
-        public static void ApplyCommonConfigurationWithPersistence(this EndpointConfiguration config, string sqlPersistenceConnectionString, string tablePrefix = null)
-        {
-            ApplyCommonConfiguration(config);
-
-            config.EnableInstallers();
-
-            var persistence = config.UsePersistence<SqlPersistence>();
-            var dialect = persistence.SqlDialect<SqlDialect.PostgreSql>();
-            if (!string.IsNullOrWhiteSpace(tablePrefix))
+            public void ApplyCommonConfiguration() 
             {
-                persistence.TablePrefix(tablePrefix);
+                config.AuditProcessedMessagesTo("audit");
+                config.SendFailedMessagesTo("error");
+
+                config.UseSerialization<NewtonsoftJsonSerializer>();
+                var rabbitCs = Environment.GetEnvironmentVariable("RABBITMQ_CONNECTION_STRING")
+                               ?? "amqp://guest:guest@localhost";
+                var transport = new RabbitMQTransport(
+                    RoutingTopology.Conventional(QueueType.Quorum), rabbitCs);
+                config.UseTransport(transport);
+
+                var messageConventions = config.Conventions();
+                messageConventions.DefiningMessagesAs(t => t.Namespace != null && t.Namespace.EndsWith(".Messages"));
+                messageConventions.DefiningEventsAs(t => t.Namespace != null && t.Namespace.EndsWith(".Messages.Events"));
+                messageConventions.DefiningCommandsAs(t => t.Namespace != null && t.Namespace.EndsWith(".Messages.Commands"));
+
+                config.SendHeartbeatTo(
+                    serviceControlQueue: "Particular.ServiceControl",
+                    frequency: TimeSpan.FromSeconds(10),
+                    timeToLive: TimeSpan.FromSeconds(5));
+
+                var metrics = config.EnableMetrics();
+                metrics.SendMetricDataToServiceControl(
+                    serviceControlMetricsAddress: "Particular.Monitoring",
+                    interval: TimeSpan.FromSeconds(5));
             }
 
-            dialect.JsonBParameterModifier(
-                modifier: parameter =>
+            public void ApplyCommonConfigurationWithPersistence(string sqlPersistenceConnectionString, string tablePrefix = null)
+            {
+                ApplyCommonConfiguration(config);
+
+                config.EnableInstallers();
+
+                var persistence = config.UsePersistence<SqlPersistence>();
+                var dialect = persistence.SqlDialect<SqlDialect.PostgreSql>();
+                if (!string.IsNullOrWhiteSpace(tablePrefix))
                 {
-                    var npgsqlParameter = (NpgsqlParameter)parameter;
-                    npgsqlParameter.NpgsqlDbType = NpgsqlDbType.Jsonb;
-                });
-            persistence.ConnectionBuilder(
-                connectionBuilder: () => new NpgsqlConnection(sqlPersistenceConnectionString));
+                    persistence.TablePrefix(tablePrefix);
+                }
 
-            persistence.EnableTransactionalSession();
+                dialect.JsonBParameterModifier(
+                    modifier: parameter =>
+                    {
+                        var npgsqlParameter = (NpgsqlParameter)parameter;
+                        npgsqlParameter.NpgsqlDbType = NpgsqlDbType.Jsonb;
+                    });
+                persistence.ConnectionBuilder(
+                    connectionBuilder: () => new NpgsqlConnection(sqlPersistenceConnectionString));
 
-            config.EnableOutbox();
+                persistence.EnableTransactionalSession();
+
+                config.EnableOutbox();
+            }
         }
     }
 }
