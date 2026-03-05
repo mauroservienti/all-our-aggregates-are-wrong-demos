ab33f7fd13d6ae107a0ad4a94a1ac03a89e91d97
Mauro Servienti <mauro.servienti@gmail.com>
2026-03-05 18:37:46 +0100

    fix the port

--- src/EndToEndTests/ATest.cs
+++ src/EndToEndTests/ATest.cs
@@ -12,13 +12,14 @@ public class ATest(ITestOutputHelper testOutput, ATestDependencies dependencies)
     public async Task ATest_that_tests_something()
     {
         var webApp = environment!.GetEndpoint("WebApp");
-        var webAppBaseUrl = webApp.GetBaseUrl(5030);
+        var webAppBaseUrl = webApp.GetBaseUrl(8080);
         var client = new HttpClient()
         {
             BaseAddress = new Uri(webAppBaseUrl)
         };
 
-        var response = await client.GetStringAsync("/");
+        var response = await client.GetAsync("/");
+        var result = await response.Content.ReadAsStringAsync();
 
         _testPassed = true;
     }
